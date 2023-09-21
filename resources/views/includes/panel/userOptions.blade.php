7d6bc0341c0e5f4edccfba9c63ff6495933f0ef5
paullcv <paul.cruz.4.pc@gmail.com>
2023-09-21 00:33:15 -0400

    Login, Registro y Dashboard añadidos

---
+++ resources/views/includes/panel/userOptions.blade.php
@@ -0,0 +1,29 @@
+<div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
+    <div class=" dropdown-header noti-title">
+        <h6 class="text-overflow m-0">Bienvenido</h6>
+    </div>
+    <a href="#" class="dropdown-item">
+        <i class="ni ni-single-02"></i>
+        <span>Mi Perfil</span>
+    </a>
+    <a href="#" class="dropdown-item">
+        <i class="ni ni-settings-gear-65"></i>
+        <span>Configuración</span>
+    </a>
+    <a href="#" class="dropdown-item">
+        <i class="ni ni-calendar-grid-58"></i>
+        <span>Mis citas</span>
+    </a>
+    <a href="#" class="dropdown-item">
+        <i class="ni ni-support-16"></i>
+        <span>Ayuda</span>
+    </a>
+    <div class="dropdown-divider"></div>
+    <a href="{{ route('logout') }}" class="dropdown-item"  onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
+        <i class="fas fa-sign-in-alt"></i>
+        <span>Cerrar Sesión</span>
+        <form action="{{ route('logout') }}" method="POST" style="display: none;" id="formLogout">
+            @csrf
+        </form>
+    </a>
+</div>
